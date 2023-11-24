8d5d0891ac1f0a0fb3c478ece7466468d84b4fc0
Your Name <you@example.com>
2023-11-24 13:22:44 +0530

    New Update a code

--- src/Page/Homepage/Homepage.tsx
+++ src/Page/Homepage/Homepage.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useRef } from "react";
+import { PDFExport } from "@progress/kendo-react-pdf";
+import { Button } from "@progress/kendo-react-buttons";
 
 function Homepage() {
-  return <div>Homepage</div>;
+  const pdfExportComponent: any = useRef();
+
+  const exportPDF = () => {
+    if (pdfExportComponent.current) {
+      pdfExportComponent.current.save();
+    }
+  };
+
+  return (
+    <div>
+      <Button onClick={exportPDF}>Export to PDF</Button>
+      <PDFExport ref={pdfExportComponent} paperSize="A4">
+        <div>
+          <h1>Hello PDF!</h1>
+          <p>This is a sample content for the PDF.</p>
+        </div>
+      </PDFExport>
+    </div>
+  );
 }
 
 export default Homepage;
